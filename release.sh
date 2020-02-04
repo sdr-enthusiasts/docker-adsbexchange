3e167fb7656a95c13409d18223b5fce74718b943
mikenye <mike.nye@xpanse.com.au>
2020-02-04 15:52:59 +0800

    Add aarch64/arm64v8 architecture

--- release.sh
+++ release.sh
@@ -8,25 +8,25 @@ IMAGE=mikenye/adsbexchange
 
 docker pull ${IMAGE}:${VERSION}-amd64
 docker pull ${IMAGE}:${VERSION}-arm32v7
-#docker pull ${IMAGE}:${VERSION}-arm64v8
+docker pull ${IMAGE}:${VERSION}-arm64v8
 
 docker manifest create --amend ${IMAGE}:${VERSION} ${IMAGE}:${VERSION}-amd64 ${IMAGE}:${VERSION}-arm32v7 #${IMAGE}:${VERSION}-arm64v8
 docker manifest annotate ${IMAGE}:${VERSION} ${IMAGE}:${VERSION}-amd64 --os linux --arch amd64
 docker manifest annotate ${IMAGE}:${VERSION} ${IMAGE}:${VERSION}-arm32v7 --os linux --arch arm --variant v7
-#docker manifest annotate ${IMAGE}:${VERSION} ${IMAGE}:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
+docker manifest annotate ${IMAGE}:${VERSION} ${IMAGE}:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
 docker manifest push --purge ${IMAGE}:${VERSION}
 
 docker tag ${IMAGE}:${VERSION}-amd64 ${IMAGE}:latest-amd64
 docker tag ${IMAGE}:${VERSION}-arm32v7 ${IMAGE}:latest-arm32v7
-#docker tag ${IMAGE}:${VERSION}-arm64v8 ${IMAGE}:latest-arm64v8
+docker tag ${IMAGE}:${VERSION}-arm64v8 ${IMAGE}:latest-arm64v8
 
 docker push ${IMAGE}:latest-amd64
 docker push ${IMAGE}:latest-arm32v7
-#docker push ${IMAGE}:latest-arm64v8
+docker push ${IMAGE}:latest-arm64v8
 
 docker manifest create --amend ${IMAGE}:latest ${IMAGE}:${VERSION}-amd64 ${IMAGE}:${VERSION}-arm32v7 #${IMAGE}:${VERSION}-arm64v8
 docker manifest annotate ${IMAGE}:latest ${IMAGE}:${VERSION}-amd64 --os linux --arch amd64
 docker manifest annotate ${IMAGE}:latest ${IMAGE}:${VERSION}-arm32v7 --os linux --arch arm --variant v7
-#docker manifest annotate ${IMAGE}:latest ${IMAGE}:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
+docker manifest annotate ${IMAGE}:latest ${IMAGE}:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
 docker manifest push --purge ${IMAGE}:latest
 
