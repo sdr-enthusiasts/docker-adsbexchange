354da208a6cff5c977ee259e034ee8a507c571e0
mikenye <mike.nye@xpanse.com.au>
2020-02-14 12:39:36 +0800

    Improvements to health monitoring script

--- scripts/health_from_adsbexchange.py
+++ scripts/health_from_adsbexchange.py
@@ -7,16 +7,19 @@ import os
 stats_raw = subprocess.check_output(['curl','--silent','https://www.adsbexchange.com/myip/'])
 stats_json = json.loads(stats_raw.decode('utf-8'))
 
+mlat_ok = False
+beast_ok = False
+
 if 'ERR' in stats_json.keys():
     print(stats_json['ERR'])
 else:
-    if stats_json['stats-uuid'].lower() == 'not found':
-        print("Statistics for feeder '%s':" % (os.environ['SITENAME']))
-    else:
-        print("Statistics for feeder '%s', UUID '%s':" % (os.environ['SITENAME'], stats_json['stats-uuid']))
-        print("Live stats available at: https://www.adsbexchange.com/api/feeders/?feed=%s" % (stats_json['stats-uuid']))
     print("")
-    print("  - Lines output: %s" % (stats_json['outlines']))
+    if stats_json['stats-uuid'].lower() == 'not found':
+        print("Feeder '%s':" % (os.environ['SITENAME']))
+    else:
+        print("Feeder '%s', UUID '%s':" % (os.environ['SITENAME'], stats_json['stats-uuid']))
+        print("Online statistics at: https://www.adsbexchange.com/api/feeders/?feed=%s" % (stats_json['stats-uuid']))
+    print("")
     datadict = dict()
     for k in stats_json.keys():
         if len(k) >= 3:
@@ -27,10 +30,24 @@ else:
                 datadict[stats_json[k]]['age'] = stats_json['age%s' % (n)]
     for k in datadict.keys():
         ks = k.split()
-        print("")
-        print("  - Data Incoming From: %s" % (ks[0])) 
-        print("  - Route:Port: %s" % (ks[1]))
-        print("  - Backend: %s" % (ks[2]))
-        print("  - Connected: %s" % (ks[3]))
-        print("  - Age: %s" % (datadict[k]['age']))
+        if k.lower().count('mlat') >= 1:
+            print("MLAT feeding OK")
+            print("  - Data Incoming From: %s" % (ks[0]))
+            print("  - Route:Port: %s" % (ks[1]))
+            print("  - Backend: %s" % (ks[2]))
+            print("  - Connected: %s" % (ks[3]))
+            print("  - Age: %s" % (datadict[k]['age']))
+            print("")
+            mlat_ok = True
+        if k.lower().count('beast') >= 1:
+            print("Mode-S Beast feeding OK")
+            print("  - Data Incoming From: %s" % (ks[0]))
+            print("  - Route:Port: %s" % (ks[1]))
+            print("  - Backend: %s" % (ks[2]))
+            print("  - Connected: %s" % (ks[3]))
+            print("  - Age: %s" % (datadict[k]['age']))
+            print("")
+            beast_ok = True
+        if not (beast_ok or mlat_ok):
+            print("No Mode-S data being received by adsbexchange, check https://adsbexchange.com/myip/")
 
