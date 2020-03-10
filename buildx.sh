dbcf689f8dfcad5200df214551189775a14a7546
mikenye <mike.nye@xpanse.com.au>
2020-03-10 23:49:08 +0800

    Automatically build & push new versions when needed

--- buildx.sh
+++ buildx.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-VERSION=20200310
-IMAGE=mikenye/adsbexchange
+VERSION=$(date +%Y%m%d)
+REPO=mikenye
+IMAGE=adsbexchange
 
-# Build the image using buildx
-docker buildx build -t ${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
-docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+# build temp image to get versions
+docker build -t ${REPO}/${IMAGE}:temp .
+docker run --rm -it --entrypoint cat ${REPO}/${IMAGE}:temp /VERSIONS > /tmp/${REPO}_${IMAGE}.current
+docker run --rm -it --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS > /tmp/${REPO}_${IMAGE}.latest
+
+# Chevk for version changes between this build and :latest
+echo ""
+echo "Version changes:"
+echo ""
+diff /tmp/${REPO}_${IMAGE}.latest /tmp/${REPO}_${IMAGE}.current 
+DIFFEXITCODE=$?
+echo ""
+
+# If versions have changed from latest image, then we rebuild latest
+if [ "$DIFFEXITCODE" -ne "0" ]
+then
+  # Build the image using buildx
+  docker buildx build -t ${REPO}/${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+  docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+else
+  echo "No version changes, nothing further to do."
+  echo ""
+fi
+
+# Clean up
+rm /tmp/${REPO}_${IMAGE}.current /tmp/${REPO}_${IMAGE}.latest
 
