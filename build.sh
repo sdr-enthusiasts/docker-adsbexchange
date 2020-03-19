0a3973f6982ba4cb8e25048cc2d4652af880ff2e
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2020-03-19 07:50:46 -0400

    linted the build shells

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 VERSION=$(date +%Y%m%d)
 ARCH=$(uname -m)
 IMAGE=mikenye/adsbexchange
 
 # Make architecture names match docker 'standards' (https://docs.docker.com/docker-for-mac/multi-arch/)
-if [ ${ARCH} = "aarch64" ]; then
+if [ "${ARCH}" = "aarch64" ]; then
     ARCH="arm64v8"
 fi
-if [ ${ARCH} = "x86_64" ]; then
+if [ "${ARCH}" = "x86_64" ]; then
     ARCH="amd64"
 fi
-if [ ${ARCH} = "armv7l" ]; then
+if [ "${ARCH}" = "armv7l" ]; then
     ARCH="arm32v7"
 fi
 
 # Build
-docker build -t ${IMAGE}:${VERSION}-${ARCH} .
-
+docker build -t "${IMAGE}:${VERSION}-${ARCH}" .
--- buildx.sh
+++ buildx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 VERSION=$(date +%Y%m%d)
 REPO=mikenye
@@ -6,22 +6,21 @@ IMAGE=adsbexchange
 
 # build temp image to get versions
 docker build -t ${REPO}/${IMAGE}:temp .
-docker run --rm --entrypoint cat ${REPO}/${IMAGE}:temp /VERSIONS > /tmp/${REPO}_${IMAGE}.current
-docker run --rm --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS > /tmp/${REPO}_${IMAGE}.latest
+docker run --rm --entrypoint cat ${REPO}/${IMAGE}:temp /VERSIONS >/tmp/${REPO}_${IMAGE}.current
+docker run --rm --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS >/tmp/${REPO}_${IMAGE}.latest
 
 # Chevk for version changes between this build and :latest
 echo ""
 echo "Version changes:"
 echo ""
-diff /tmp/${REPO}_${IMAGE}.latest /tmp/${REPO}_${IMAGE}.current 
+diff /tmp/${REPO}_${IMAGE}.latest /tmp/${REPO}_${IMAGE}.current
 DIFFEXITCODE=$?
 echo ""
 
 # If versions have changed from latest image, then we rebuild latest
-if [ "$DIFFEXITCODE" -ne "0" ]
-then
+if [ "$DIFFEXITCODE" -ne "0" ]; then
   # Build the image using buildx
-  docker buildx build -t ${REPO}/${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+  docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
   docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
 else
   echo "No version changes, nothing further to do."
@@ -30,4 +29,3 @@ fi
 
 # Clean up
 rm /tmp/${REPO}_${IMAGE}.current /tmp/${REPO}_${IMAGE}.latest
-
