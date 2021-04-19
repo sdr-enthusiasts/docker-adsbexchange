3ae750a62e207d680046ea92892362eeeb796b56
Mike <mike.nye@gmail.com>
2021-04-19 10:32:13 +0800

    Fixes for Issue 52 (#53)
    * update to support stats-uuid1
    
    * Fix missed json keys

--- rootfs/scripts/health_from_adsbexchange.py
+++ rootfs/scripts/health_from_adsbexchange.py
@@ -3,6 +3,7 @@
 import subprocess
 import json
 import os
+import sys
 
 stats_raw = subprocess.check_output(['curl','--silent','https://www.adsbexchange.com/myip/'])
 stats_json = json.loads(stats_raw.decode('utf-8'))
@@ -14,11 +15,20 @@ if 'ERR' in stats_json.keys():
     print(stats_json['ERR'])
 else:
     print("")
-    if stats_json['stats-uuid'].lower() == 'not found':
+    
+    if 'stats-uuid' in stats_json.keys():
+        jsonkey = 'stats-uuid'
+    elif 'stats-uuid1' in stats_json.keys():
+        jsonkey = 'stats-uuid1'
+    else:
+        print("Could not determine UUID")
+        sys.exit(1)
+    
+    if stats_json[jsonkey].lower() == 'not found':
         print("Feeder '%s':" % (os.environ['SITENAME']))
     else:
-        print("Feeder '%s', UUID '%s':" % (os.environ['SITENAME'], stats_json['stats-uuid']))
-        print("Online statistics at: https://www.adsbexchange.com/api/feeders/?feed=%s" % (stats_json['stats-uuid']))
+        print("Feeder '%s', UUID '%s':" % (os.environ['SITENAME'], stats_json[jsonkey]))
+        print("Online statistics at: https://www.adsbexchange.com/api/feeders/?feed=%s" % (stats_json[jsonkey]))
     print("")
     datadict = dict()
     for k in stats_json.keys():
